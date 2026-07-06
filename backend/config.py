de6d3f9db230875f6939fc56c10aceb63b5880dd
moonstack20 <sheebshak@gmail.com>
2026-07-07 00:02:42 +0530

    Day 1 task is completed -backend and frontend fully set up

---
+++ backend/config.py
@@ -0,0 +1,14 @@
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+class Config:
+    SECRET_KEY = os.getenv("SECRET_KEY", "super-secret-key")
+    JWT_SECRET_KEY = os.getenv("JWT_SECRET_KEY", "jwt-secret-key")
+    SQLALCHEMY_DATABASE_URI = os.getenv(
+        "DATABASE_URL",
+        "sqlite:///database.db"
+    )
+    SQLALCHEMY_TRACK_MODIFICATIONS = False
+    
